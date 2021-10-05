af2cc0b6b244a2359eb52bdabaa30709d7cf5d4c
lshep <lori.shepherd@roswellpark.org>
2021-10-05 09:35:46 -0400

    user requested deprecation

--- R/zzz.R
+++ R/zzz.R
@@ -2,4 +2,10 @@
     if (!hasGO()) {
         packageStartupMessage("No gene ontology file detected. Run 'getGO()' to download latest version")
     }
+    msg <- sprintf(
+        "Package '%s' is deprecated and will be removed from Bioconductor
+         version %s", pkgname, "3.15")
+    .Deprecated(msg=paste(strwrap(msg, exdent=2), collapse="\n"))
+
+    
 }
